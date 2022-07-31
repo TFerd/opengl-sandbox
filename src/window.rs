e4e22bac7a82ff38fb7340b2f280b72684ea3195
Thomas Ferdinand <thomasferdinand96@gmail.com>
2022-07-31 12:42:07 -0400

    now uses our custom opengl library crate

--- src/window.rs
+++ src/window.rs
@@ -1,10 +1,13 @@
 extern crate gl;
 extern crate glfw;
+//extern crate opengl;
 
-use core::mem::{size_of, size_of_val};
+use core::mem::size_of;
 
-//use gl::types::*;
 use glfw::{Action, Context, Key};
+use opengl::buffer::*;
+use opengl::shader::*;
+use opengl::vertex_array::*;
 
 type Vertex = [f32; 3];
 const VERTICES: [Vertex; 4] = [
@@ -73,44 +76,34 @@ pub fn create_window() {
     //????????
     window.set_framebuffer_size_polling(true);
 
+    opengl::clear_color(1.0, 0.2, 0.5, 1.0);
+
+    //Generate a Vertex Array Object(VAO)
+    let vao = VertexArray::new().expect("Failed to create VAO");
+    vao.bind();
+
+    //Generate Vertex Buffer Objects (VBOs)
+    let vbo = Buffer::new().expect("Failed to create VBO");
+    vbo.bind(BufferType::Array);
+    Buffer::buffer_data(
+        BufferType::Array,
+        bytemuck::cast_slice(&VERTICES),
+        gl::STATIC_DRAW,
+    );
+
+    //Create Element Buffer Object (EBO)
+    let ebo = Buffer::new().expect("Failed to create EBO");
+    ebo.bind(BufferType::ElementArray);
+    Buffer::buffer_data(
+        BufferType::ElementArray,
+        bytemuck::cast_slice(&INDICES),
+        gl::STATIC_DRAW,
+    );
+
+    let program = ShaderProgram::create_program_from_src(VERT_SHADER, FRAG_SHADER).unwrap();
+    program.use_program();
+
     unsafe {
-        gl::ClearColor(1.0, 0.2, 0.5, 1.0);
-
-        //Generate a Vertex Array Object(VAO)
-        /*
-        let mut vao: u32 = 0;
-        gl::GenVertexArrays(1, &mut vao);
-        assert_ne!(vao, 0);
-        gl::BindVertexArray(vao);
-        */
-        let vao = opengl::VertexArray::new().expect("Couldn't make a VAO");
-        vao.bind();
-
-        //Generate Vertex Buffer Objects (VBOs)
-        let mut vbo: u32 = 0;
-        gl::GenBuffers(1, &mut vbo);
-        assert_ne!(vbo, 0);
-        gl::BindBuffer(gl::ARRAY_BUFFER, vbo); //Set this buffer (vbo) as active, meaning
-        gl::BufferData(
-            //this is where we will be putting data next (i think)
-            gl::ARRAY_BUFFER,
-            size_of_val(&VERTICES) as isize,
-            VERTICES.as_ptr().cast(),
-            gl::STATIC_DRAW,
-        );
-
-        //Create Element Buffer Object (EBO)
-        let mut ebo = 0;
-        gl::GenBuffers(1, &mut ebo);
-        assert_ne!(ebo, 0);
-        gl::BindBuffer(gl::ELEMENT_ARRAY_BUFFER, ebo);
-        gl::BufferData(
-            gl::ELEMENT_ARRAY_BUFFER,
-            size_of_val(&INDICES) as isize,
-            INDICES.as_ptr().cast(),
-            gl::STATIC_DRAW,
-        );
-
         //Vertex Attribute
         gl::VertexAttribPointer(
             0, //bc we put: layout (location = 0) in our vertex code
@@ -122,86 +115,6 @@ pub fn create_window() {
         );
 
         gl::EnableVertexAttribArray(0);
-
-        //Create Vertex Shader
-        let vertex_shader = gl::CreateShader(gl::VERTEX_SHADER);
-        assert_ne!(vertex_shader, 0);
-        gl::ShaderSource(
-            vertex_shader,
-            1,
-            &(VERT_SHADER.as_bytes().as_ptr().cast()),
-            &(VERT_SHADER.len().try_into().unwrap()),
-        );
-        gl::CompileShader(vertex_shader);
-        let mut success = 0;
-        gl::GetShaderiv(vertex_shader, gl::COMPILE_STATUS, &mut success);
-
-        //Check vertex shader compile success
-        if success == 0 {
-            let mut v: Vec<u8> = Vec::with_capacity(1024);
-            let mut log_len = 0_i32;
-            gl::GetShaderInfoLog(vertex_shader, 1024, &mut log_len, v.as_mut_ptr().cast());
-            v.set_len(log_len.try_into().unwrap());
-            panic!("Vertex Compile Error: {}", String::from_utf8_lossy(&v));
-
-            /*
-            let buffer_size = gl::INFO_LOG_LENGTH;
-            let mut v: Vec<u8> = Vec::with_capacity(buffer_size.try_into().unwrap());
-            let mut log_len = 0_i32;
-
-            gl::GetShaderInfoLog(
-                vertex_shader,
-                buffer_size.try_into().unwrap(),
-                &mut log_len,
-                v.as_mut_ptr().cast(),
-            );
-            v.set_len(log_len.try_into().unwrap());
-            panic!("Vertex Compile Error: {}", String::from_utf8_lossy(&v)); */
-        } //End Vertex Shader
-
-        //Create Fragment Shader
-        let fragment_shader = gl::CreateShader(gl::FRAGMENT_SHADER);
-        assert_ne!(fragment_shader, 0);
-        gl::ShaderSource(
-            fragment_shader,
-            1,
-            &(FRAG_SHADER.as_bytes().as_ptr().cast()),
-            &(FRAG_SHADER.len().try_into().unwrap()),
-        );
-        gl::CompileShader(fragment_shader);
-
-        //Check fragment shader compile success
-        let mut success = 0;
-        gl::GetShaderiv(fragment_shader, gl::COMPILE_STATUS, &mut success);
-        if success == 0 {
-            let mut v: Vec<u8> = Vec::with_capacity(1024);
-            let mut log_len = 0_i32;
-            gl::GetShaderInfoLog(fragment_shader, 1024, &mut log_len, v.as_mut_ptr().cast());
-            v.set_len(log_len.try_into().unwrap());
-            panic!("Fragment Compile Error: {}", String::from_utf8_lossy(&v));
-        } //End fragment shader
-
-        //Create program
-        let shader_program = gl::CreateProgram();
-        gl::AttachShader(shader_program, vertex_shader);
-        gl::AttachShader(shader_program, fragment_shader);
-        gl::LinkProgram(shader_program);
-
-        //Check for link error
-        let mut success = 0;
-        gl::GetProgramiv(shader_program, gl::LINK_STATUS, &mut success);
-        if success == 0 {
-            let mut v: Vec<u8> = Vec::with_capacity(1024);
-            let mut log_len = 0_i32;
-            gl::GetShaderInfoLog(shader_program, 1024, &mut log_len, v.as_mut_ptr().cast());
-            v.set_len(log_len.try_into().unwrap());
-            panic!("Program Link Error: {}", String::from_utf8_lossy(&v));
-        } //End create program
-
-        gl::DeleteShader(vertex_shader);
-        gl::DeleteShader(fragment_shader);
-
-        gl::UseProgram(shader_program);
     }
 
     let mut wireframe_mode = false;
