c06d3395185cf72086d9dc313933ecaca1e60535
Thomas Ferdinand <thomasferdinand96@gmail.com>
2022-08-02 22:09:18 -0400

    move process inputs to separate function

--- src/window.rs
+++ src/window.rs
@@ -3,20 +3,25 @@ extern crate glfw;
 //extern crate opengl;
 
 use core::mem::size_of;
+use std::sync::mpsc::Receiver;
 
 use glfw::{Action, Context, Key};
 use opengl::buffer::*;
 use opengl::shader::*;
 use opengl::vertex_array::*;
+use opengl::*;
 
 type Vertex = [f32; 3];
 const VERTICES: [Vertex; 4] = [
-    [0.5, -0.5, 0.0],
+    // A rectangle has 4 points
+    [0.5, 0.5, 0.0],
     [0.5, -0.5, 0.0],
     [-0.5, -0.5, 0.0],
     [-0.5, 0.5, 0.0],
 ];
-const INDICES: [u32; 6] = [0, 1, 3, 1, 2, 3];
+
+type TriIndexes = [u32; 3];
+const INDICES: [TriIndexes; 2] = [[0, 1, 3], [1, 2, 3]];
 
 const VERT_SHADER: &str = r#"#version 330 core
   layout (location = 0) in vec3 pos;
@@ -37,33 +42,35 @@ const FRAG_SHADER: &str = r#"#version 330 core
 //    create_window();
 //}
 
-//TODO: handle inputs
+// TODO: handle inputs
 pub fn create_window() {
     let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
 
-    glfw.window_hint(glfw::WindowHint::ContextVersion(3, 3)); //use OpenGL version 3.3 i think?
+    glfw.window_hint(glfw::WindowHint::ContextVersion(3, 3)); // use OpenGL version 3.3 i think?
     glfw.window_hint(glfw::WindowHint::OpenGlProfile(
         //???
         glfw::OpenGlProfileHint::Core,
     ));
     glfw.window_hint(glfw::WindowHint::OpenGlForwardCompat(true));
 
-    //Creates window and its context?
+    // Creates window and its context?
     let (mut window, events) = glfw
         .create_window(800, 640, "Title", glfw::WindowMode::Windowed)
         .expect("Failed to create window!");
 
-    //Make the windows context current?
+    // Make the windows context current?
     window.make_current();
     window.set_key_polling(true);
 
-    //Set VSync
+    window.set_framebuffer_size_polling(true);
+
+    // Set VSync
     glfw.set_swap_interval(glfw::SwapInterval::Sync(1));
 
-    //Loads all functions pointers
+    // Loads all functions pointers
     gl::load_with(|s| glfw.get_proc_address_raw(s));
 
-    //Check if function is loaded before trying to use
+    // Check if function is loaded before trying to use
     if gl::Viewport::is_loaded() {
         println!("Viewport is loaded");
         unsafe {
@@ -78,11 +85,11 @@ pub fn create_window() {
 
     opengl::clear_color(1.0, 0.2, 0.5, 1.0);
 
-    //Generate a Vertex Array Object(VAO)
+    // Generate a Vertex Array Object(VAO)
     let vao = VertexArray::new().expect("Failed to create VAO");
     vao.bind();
 
-    //Generate Vertex Buffer Objects (VBOs)
+    // Generate Vertex Buffer Objects (VBOs)
     let vbo = Buffer::new().expect("Failed to create VBO");
     vbo.bind(BufferType::Array);
     Buffer::buffer_data(
@@ -91,7 +98,7 @@ pub fn create_window() {
         gl::STATIC_DRAW,
     );
 
-    //Create Element Buffer Object (EBO)
+    // Create Element Buffer Object (EBO)
     let ebo = Buffer::new().expect("Failed to create EBO");
     ebo.bind(BufferType::ElementArray);
     Buffer::buffer_data(
@@ -100,14 +107,11 @@ pub fn create_window() {
         gl::STATIC_DRAW,
     );
 
-    let program = ShaderProgram::create_program_from_src(VERT_SHADER, FRAG_SHADER).unwrap();
-    program.use_program();
-
     unsafe {
-        //Vertex Attribute
+        // Vertex Attribute
         gl::VertexAttribPointer(
-            0, //bc we put: layout (location = 0) in our vertex code
-            3, //bc our vertex is a vec3 (x, y, z)
+            0, // bc we put: layout (location = 0) in our vertex code
+            3, // bc our vertex is a vec3 (x, y, z)
             gl::FLOAT,
             gl::FALSE,
             size_of::<Vertex>().try_into().unwrap(),
@@ -117,55 +121,62 @@ pub fn create_window() {
         gl::EnableVertexAttribArray(0);
     }
 
-    let mut wireframe_mode = false;
-    //Loop until window is closed
-    while !window.should_close() {
-        //Handle inputs
-        for (_, event) in glfw::flush_messages(&events) {
-            println!("{:?}", event);
-            match event {
-                //Esc | Close window
-                glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
-                    //Closes window when ESC is pressed i think? yes
-                    window.set_should_close(true);
-                }
-                //Spacebar | Toggle wireframe mode
-                glfw::WindowEvent::Key(Key::Space, _, Action::Press, _) => {
-                    println!("Wireframe mode is currently: {}", wireframe_mode);
-                    if !wireframe_mode {
-                        unsafe {
-                            gl::PolygonMode(gl::FRONT_AND_BACK, gl::LINE);
-                        }
-                        wireframe_mode = true;
-                    } else {
-                        unsafe {
-                            gl::PolygonMode(gl::FRONT_AND_BACK, gl::FILL);
-                        }
-                        wireframe_mode = false;
-                    }
-                }
-                _ => {} //otherwise continue/do nothing
-            }
-        } //End input handler
+    let program = ShaderProgram::create_program_from_src(VERT_SHADER, FRAG_SHADER).unwrap(); //unwrap bc its a Result
+    program.use_program();
 
-        //Rendering commands here!?
+    let mut wireframe_mode = false;
+    /*********************************************************************************************************
+    @TODO: https://learnopengl.com/Getting-started/Hello-Triangle
+    This says that UseProgram, BindVertexArray, DrayElements, and BindVertexArray should be in the render loop
+    and everything else should be in the initialization (before the loop)
+    **********************************************************************************************************/
+    // Rendering Loop:
+    while !window.should_close() {
+        // Handle inputs
+        process_events(&mut window, &events, &mut wireframe_mode);
+
+        // Rendering commands here!?
+        // Yes, once the events are clear, we can change the world state.
+        // Then, draw
 
         unsafe {
             gl::Clear(gl::COLOR_BUFFER_BIT);
-
-            //useprogram
-            //bindvertexarray(VAO[0])
-            //drawArrays(gltriangles,0,3);
-            //bindvertexarray(vao[1])
-            //drawarrays(gltriangles,0,3);
-
             gl::DrawElements(gl::TRIANGLES, 6, gl::UNSIGNED_INT, 0 as *const _);
         }
 
-        //poll for and process events ??
+        // poll for and process events ??
         glfw.poll_events();
-        //Swap front and back buffers (wtf?)
+        // Swap front and back buffers (wtf?)
         window.swap_buffers();
+    }
+}
+
+fn process_events(
+    window: &mut glfw::Window,
+    events: &Receiver<(f64, glfw::WindowEvent)>,
+    wireframe_mode: &mut bool, // Get this outta here, maybe pass in an options object
+) {
+    for (_, event) in glfw::flush_messages(events) {
+        match event {
+            glfw::WindowEvent::FramebufferSize(w, h) => unsafe { gl::Viewport(0, 0, w, h) },
+
+            // Esc | Close window
+            glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
+                // Closes window when ESC is pressed i think? yes
+                window.set_should_close(true);
+            }
+            // Spacebar | Toggle wireframe mode
+            glfw::WindowEvent::Key(Key::Space, _, Action::Press, _) => {
+                if *wireframe_mode == false {
+                    opengl::polygon_mode(PolygonMode::Line);
+                    *wireframe_mode = true;
+                } else {
+                    opengl::polygon_mode(PolygonMode::Fill);
+                    *wireframe_mode = false;
+                }
+            }
+            _ => {} // otherwise continue/do nothing
+        }
     }
 }
 
